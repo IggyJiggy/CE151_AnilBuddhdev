8b5d77e759cf626aeaa4ecaa7b2b239fa0280674
Anil Buddhdev <abuddhdev20@gmail.com>
2021-07-09 16:08:22 +0530

    Comments

---
+++ lab1.py
@@ -0,0 +1,5 @@
+import nltk, scipy, numpy, matplotlib, pandas
+# nltk has Natural Language Tool Kit to process Text Data
+# Scipy is used to provide scientific calculations necessary for machine applications
+# Numpy provide methods for Numerical calculations/ Linear Algebra
+# MatplotLib for graph representation
