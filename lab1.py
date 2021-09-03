6a9fb3a9325f203b1856daef5d7a203deb39db37
IggyJiggy <46241746+IggyJiggy@users.noreply.github.com>
2021-09-03 08:18:05 +0530

    Delete lab1.py

--- lab1.py
+++
@@ -1,5 +0,0 @@
-import nltk, scipy, numpy, matplotlib, pandas
-# nltk has Natural Language Tool Kit to process Text Data
-# Scipy is used to provide scientific calculations necessary for machine applications
-# Numpy provide methods for Numerical calculations/ Linear Algebra
-# MatplotLib for graph representation
